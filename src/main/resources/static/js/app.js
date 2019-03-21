04c7eabb6c948cc14c790c55ded673793d09fda4
alejandrordto <masterale2011@gmail.com>
2019-03-20 21:03:51 -0500

    first

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,22 +1,43 @@
 var app = (function () {
     var cinema;
-    var functions;
-    var getCinema =function(){
-        var name= $("#cinema").val(); 
-        var callback= function(cine){
-                     var table= $("table tbody").find('input[name="table"]');
-                     
-		}
-        var funciones =function(cine){
-            
-             return cine[0].functions;
+    var functions = [];
+    function recargar(cine) {
+        for (var i = 0; i < functions.length; i++) {
+            var numsit= count(functions[i].seats)
+            var flag = "<tr><td>" + cine[0].name + "</td><td>" +
+                    functions[i].movie.name + "</td><td>" + numsit + "</td><td>" +
+                    functions[i].date + "</td></tr>";
+            $("#tabla").append(flag);
         }
-        var func = apimock.getCinemaByName(name,funciones);
-           console.log()
+        function count(sit) {
+            var cont = 0;
+            for (var i = 0; i < sit.length; i++) {
+                for (var j = 0; j < sit[i].length; j++) {
+                    if (sit[i][j] == true) {
+                        cont++;
+                    }
+                }
+            }
+            return cont;
         }
-       
+    }
+    var getCinema = function () {
+        var name = $("#cinema").val();
+        var callback = function (cine) {
+            cinema = cine;
+
+        }
+        var funciones = function (cine) {
+            functions = cine[0].functions;
+            recargar(cine);
+            return functions;
+        }
+        var func = apimock.getCinemaByName(name, funciones);
+        console.log()
+    }
+
     return{
-    getCinema:getCinema
-};
-    
+        getCinema: getCinema
+    };
+
 })();
